9fbc41e054893cfb7247fe55c1f7b9d635f8243e
Erik Ljungdahl <ekke.ekke@gmail.com>
2025-12-11 20:03:24 +0100

    feat: day 1 part two

--- 1/main.go
+++ 1/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	content, err := os.ReadFile("inputs/1.txt")
+	content, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,14 +16,32 @@ func main() {
 	lines := strings.Split(string(content), "\n")
 
 	var dial int = 50
+	var newDial int = 50
 
 	var atZero int
 
+	var subSteps []int
+
 	for _, line := range lines {
+
 		n, err := strconv.Atoi(line[1:])
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// part 2
+		for i := 1; i <= n; i++ {
+			if line[0] == 'R' {
+				newDial = newDial + 1
+			} else {
+				newDial = newDial - 1
+			}
+
+			subSteps = append(subSteps, newDial)
+
+		}
+
+		// part 1
 		if line[0] == 'R' {
 			dial += n
 		} else {
@@ -36,4 +54,22 @@ func main() {
 	}
 
 	println(atZero)
+	atZero = 0
+
+	for _, line := range subSteps {
+		if mod100(line) == 0 {
+			atZero += 1
+		}
+	}
+
+	println(atZero)
+
+}
+
+func mod100(a int) int {
+	m := a % 100
+	if m < 0 {
+		return -m
+	}
+	return m
 }
