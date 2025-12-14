bef5704389be2c2138e288cc87fd2a9c098f8d51
ErikLjungdahl <ekke.ekke@gmail.com>
2025-12-14 20:25:39 +0100

    feat: day 3 part two

--- 3/main.go
+++ 3/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -16,6 +17,7 @@ func main() {
 
 	lines := strings.Split(string(content), "\n")
 
+	// part one
 	var sum int = 0
 	for _, line := range lines {
 		var highest string = "00"
@@ -29,4 +31,28 @@ func main() {
 
 	}
 	fmt.Println(sum)
+
+	// part two
+	var sum2 int = 0
+	for _, line := range lines {
+		var highest string = line[0:12]
+
+		for i := 1; i < len(line); i++ {
+			var options []string
+			// 0, if len(line) - i >= 12
+			// 1-11 if len(line) - i <= 12
+			// max(0, len(line)-12+i)
+			for j := max(0, 12-(len(line)-i)); j < min(12, i); j++ {
+				options = append(options, highest[0:j]+line[i:i+1]+strings.Repeat("0", max(11-j, 0)))
+			}
+			options = append(options, highest)
+			highest = slices.Max(options)
+		}
+
+		fmt.Println(highest)
+		n, _ := strconv.Atoi(highest)
+		sum2 += n
+
+	}
+	fmt.Println(sum2)
 }
