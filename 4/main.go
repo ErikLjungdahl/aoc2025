1ab230a3f8216d48967da496bdb9ddfcecc07d2b
ErikLjungdahl <ekke.ekke@gmail.com>
2025-12-14 21:31:26 +0100

    feat: day 4 part two

--- 4/main.go
+++ 4/main.go
@@ -22,24 +22,49 @@ func main() {
 	lines = append(lines, strings.Repeat(".", length))
 	lines = append([]string{strings.Repeat(".", length)}, lines...)
 
-	for _, line := range lines {
-		println(line)
-	}
+	// for _, line := range lines {
+	// 	println(line)
+	// }
 
 	var accessible int
-	for y := 1; y < len(lines)-1; y++ {
+	var last int
+	for i := 0; ; i++ {
 
-		for x := 1; x < len(lines[0])-1; x++ {
-			if lines[y][x] == '@' {
-				count := strings.Count(lines[y-1][x-1:x+2]+lines[y][x-1:x+2]+lines[y+1][x-1:x+2], "@")
-				println(y, x, count, lines[y-1][x-1:x+2]+lines[y][x-1:x+2]+lines[y+1][x-1:x+2])
-				if count < 5 {
-					accessible++
-					println(y, x)
+		for y := 1; y < len(lines)-1; y++ {
+
+			for x := 1; x < len(lines[0])-1; x++ {
+				if lines[y][x] == '@' {
+					countAt := strings.Count(lines[y-1][x-1:x+2]+lines[y][x-1:x+2]+lines[y+1][x-1:x+2], "@")
+					countX := strings.Count(lines[y-1][x-1:x+2]+lines[y][x-1:x+2]+lines[y+1][x-1:x+2], "x")
+					count := countAt + countX
+					// println(y, x, count, lines[y-1][x-1:x+2]+lines[y][x-1:x+2]+lines[y+1][x-1:x+2])
+					if count < 5 {
+						accessible++
+						// println(y, x)
+						lines[y] = lines[y][0:x] + "x" + lines[y][x+1:len(lines[0])]
+					}
+				}
+			}
+		}
+
+		if i == 0 {
+			println(accessible)
+		}
+
+		if accessible == last {
+			println(accessible)
+			break
+		}
+		last = accessible
+
+		for y := 1; y < len(lines)-1; y++ {
+
+			for x := 1; x < len(lines[0])-1; x++ {
+				if lines[y][x] == 'x' {
+					lines[y] = lines[y][0:x] + "." + lines[y][x+1:len(lines[0])]
 				}
 			}
 		}
 	}
 
-	println(accessible)
 }
