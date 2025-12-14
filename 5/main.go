3ed5f8cbfaf28ac9559c20e3dd49b6e32ca1b205
ErikLjungdahl <ekke.ekke@gmail.com>
2025-12-14 23:48:31 +0100

    feat: day 5 part two

--- 5/main.go
+++ 5/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+type rang struct {
+	start int
+	end   int
+}
 
 func main() {
 	content, err := os.ReadFile(os.Args[1])
@@ -40,5 +46,52 @@ func main() {
 	}
 
 	println(freshCount)
+
+	println("part 2")
+	var intRanges []rang
+
+	for _, r := range freshRanges {
+		r := strings.Split(r, "-")
+		left, _ := strconv.Atoi(r[0])
+		right, _ := strconv.Atoi(r[1])
+
+		rang := rang{start: left, end: right}
+		intRanges = append(intRanges, rang)
+	}
+
+	sort.Slice(intRanges, func(i, j int) bool { return intRanges[i].start < intRanges[j].start })
+
+	var newRanges []rang
+
+	var currStart int = intRanges[0].start
+	var currEnd int = intRanges[0].end
+
+	for _, rng := range intRanges[1:] {
+		// println(rng.start)
+
+		if rng.end < currEnd {
+			continue
+		}
+
+		if rng.start > currEnd {
+			rang := rang{start: currStart, end: currEnd}
+			newRanges = append(newRanges, rang)
+			currStart = rng.start
+		}
+		currEnd = rng.end
+
+	}
+	rang := rang{start: currStart, end: currEnd}
+	newRanges = append(newRanges, rang)
+
+	println("newRanges", len(newRanges))
+
+	var sum int
+	for _, rng := range newRanges {
+		// println(rng.start, rng.end)
+		sum += rng.end + 1 - rng.start
+	}
+
+	println(sum)
 
 }
